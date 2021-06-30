f8828639f53049092482e7f2961627f1418633ba
CodingWillow <64945652+CodingWillow@users.noreply.github.com>
2021-06-30 10:37:34 +0900

    Update info_missing.py
    The function returns nothing if there is no missing data,
    and else it returns the names, index numbers, count, and ratios of the columns with missing data.

--- data_exploration/info_missing.py
+++ data_exploration/info_missing.py
@@ -6,21 +6,21 @@ def info_missing(df):
     if len(df[all_cols[i]].dropna()) < len(df):
       features_w_na.append(all_cols[i])
 
-  print('<Features with missing values>')
-  print(f'Count: %d features' %len(features_w_na))
-  print()
-  print(' #   column       na count   na ratio')
-  print('---  ------       --------   --------')
+  if( len(features_w_na) == 0):
+    print("This DataFrame has no missing data.")
 
-  n_features = len(features_w_na)
-  for i in range(0, n_features):
-    na_name = features_w_na[i]
-    na_index = list(all_cols).index(na_name)
-    na_count = df[na_name].isna().sum()
-    na_ratio = round(na_count / len(df), 3)
-    print("{:<4} {:<15} {:5} {:10}".format(na_index, na_name, na_count, na_ratio))
+  else: 
+    print('<Features with missing values>')
+    print(f'Count: %d features' %len(features_w_na))
+    print()
+    print(' #   column       na count   na ratio')
+    print('---  ------       --------   --------')
 
-info_missing(df)
-
-#    print(f' %d   %s' %(list(list(all_cols)).index(na_name), na_name))    
-#print ("{5:<} {:<15} {:<10}".format('Name','Age','Percent'))
+    n_features = len(features_w_na)
+    for i in range(0, n_features):
+      na_name = features_w_na[i]
+      na_index = list(all_cols).index(na_name)
+      na_count = df[na_name].isna().sum()
+      na_ratio = round(na_count / len(df), 3)
+      print("{:<4} {:<15} {:5} {:10}".format(na_index, na_name, na_count, na_ratio))
+      return na_name, na_index, na_count, na_ratio
